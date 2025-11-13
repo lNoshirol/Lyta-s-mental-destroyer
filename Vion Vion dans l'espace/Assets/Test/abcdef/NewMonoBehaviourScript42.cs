6f0af030fabfeb8ab14f78b1713ad1217a6c6d28
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2025-11-13 10:02:24 +0100

    tbsgrth

---
+++ Vion Vion dans l'espace/Assets/Test/abcdef/NewMonoBehaviourScript42.cs
@@ -0,0 +1,181 @@
+using UnityEngine;
+
+
+
+
+/*
+ Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster, monster, monster, monster
+Mon-mon-mon-mon-mon-monster
+Monster
+How should I feel?
+Monster
+How should I feel?
+
+Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster
+How should I feel?
+Creatures lie here
+Looking throu-ou-ou-ou-ou-ough
+
+Monster
+How should I fe-e-e-e-e-el?
+Creatures lie here
+Looking through the window
+
+Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster, monster, monster, monster
+Mon-mon-mon-mon-mon-monster
+Monster
+How should I feel?
+Monster
+How should I feel?
+ */
+
+public class NewMonoBehaviourScript42 : MonoBehaviour
+{
+
+
+
+    /*
+     Monster
+    How should I feel?
+    Creatures lie here
+    Looking through the window
+
+    Monster
+    How should I feel?
+    Creatures lie here
+    Looking through the window
+
+    Monster
+    How should I feel?
+    Creatures lie here
+    Looking through the window
+
+    Monster
+    How should I feel?
+    Creatures lie here
+    Looking through the window
+
+    Monster, monster, monster, monster
+    Mon-mon-mon-mon-mon-monster
+    Monster
+    How should I feel?
+    Monster
+    How should I feel?
+
+    Monster
+    How should I feel?
+    Creatures lie here
+    Looking through the window
+
+    Monster
+    How should I feel?
+    Creatures lie here
+    Looking throu-ou-ou-ou-ou-ough
+
+    Monster
+    How should I fe-e-e-e-e-el?
+    Creatures lie here
+    Looking through the window
+
+    Monster
+    How should I feel?
+    Creatures lie here
+    Looking through the window
+
+    Monster, monster, monster, monster
+    Mon-mon-mon-mon-mon-monster
+    Monster
+    How should I feel?
+    Monster
+    How should I feel?
+     */
+}
+
+
+
+
+/*
+ Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster, monster, monster, monster
+Mon-mon-mon-mon-mon-monster
+Monster
+How should I feel?
+Monster
+How should I feel?
+
+Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster
+How should I feel?
+Creatures lie here
+Looking throu-ou-ou-ou-ou-ough
+
+Monster
+How should I fe-e-e-e-e-el?
+Creatures lie here
+Looking through the window
+
+Monster
+How should I feel?
+Creatures lie here
+Looking through the window
+
+Monster, monster, monster, monster
+Mon-mon-mon-mon-mon-monster
+Monster
+How should I feel?
+Monster
+How should I feel?
+ */
