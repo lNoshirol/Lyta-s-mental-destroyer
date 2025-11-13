8a4825674f10e0efb34a1f34fbe09cf32b6fadfc
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2025-11-13 10:04:36 +0100

    frsqf

---
+++ Vion Vion dans l'espace/Assets/New Folder/SuperMovementScriptToutParfaitSansAucuneErreurDeNomenclatureWallahJAIMEPASLESNOIRSMaisBonCestPasLaQuestionFinBrefJePenseQueCestAssezLongCommeCaMaisJsuisPasSur.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class SuperMovementScriptToutParfaitSansAucuneErreurDeNomenclatureWallahJAIMEPASLESNOIRSMaisBonCestPasLaQuestionFinBrefJePenseQueCestAssezLongCommeCaMaisJsuisPasSur : MonoBehaviour
+{
+    public GameObject PereCastorRacontenouuuuuuuuuuuuuuuuuuuuuuuuuuuuusUuuuuuneHistoire;
+
+    public GameObject l2;
+
+    private void Update()
+    {
+
+
+    }
+
+    public void MagicFunction1()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Application.Quit();
+        }
+        else
+        {
+            MagicFunction6();
+        }
+    }
+
+    public void MagicFunction2()
+    {
+
+    }
+
+    public void MagicFunction3() 
+    { 
+
+    }
+    public void MagicFunction4() 
+    {
+        
+    }
+    public void MagicFunction5() 
+    {
+
+    }
+    public void MagicFunction6() 
+    {
+        
+    }
+    public void MagicFunction7() 
+    {
+        
+    }
+    public void MagicFunction8() 
+    {
+        
+    }
+    public void MagicFunction9() 
+    {
+        
+    } 
+    public void MagicFunction10() 
+    {
+        
+    }
+}
